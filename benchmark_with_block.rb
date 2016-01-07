0a0fdd89cf3dc7bd35a0506ad79835330982cd44
choconnie <hj.c.cho@gmail.com>
2016-01-07 15:58:55 +0000

    test for running time

---
+++ benchmark_with_block.rb
@@ -0,0 +1,15 @@
+# Be careful, pasting this into IRB/Pry will take a long time to print.
+# It's a loooong string. :)
+
+def benchmark
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  end_time - start_time
+end
+
+long_string = "apple" * 100000000
+
+running_time = benchmark { long_string.reverse }
+
+puts "string.reverse took #{running_time} seconds to run"
